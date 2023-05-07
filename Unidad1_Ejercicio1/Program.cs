3ae1ed8105b7592f0baca6f40898f8f7a1c41cfa
ACB072 <antoniomiralless@gmail.com>
2023-05-07 19:13:51 +0200

    Ejercicio 1 Unidad 1

---
+++ Unidad1_Ejercicio1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
