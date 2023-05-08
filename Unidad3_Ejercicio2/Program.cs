abce68ba8da197a64eed94e40a67389501decf76
Antonio Miralles Gutierrez <65496826+ACB072@users.noreply.github.com>
2023-05-08 09:53:45 +0200

    Delete Unidad3_Ejercicio2 directory

--- Unidad3_Ejercicio2/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿int saleAmount = 1001;
-int discount = saleAmount > 1000 ? 100 : 50;
-Console.WriteLine($"Discount: {discount}");
-
-saleAmount = 1001;
-// int discount = saleAmount > 1000 ? 100 : 50;
-
-Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
