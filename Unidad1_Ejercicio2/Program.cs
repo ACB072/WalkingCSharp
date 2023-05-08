de44ecb02580edb93db2d8c97c053c26009da27e
Antonio Miralles Gutierrez <65496826+ACB072@users.noreply.github.com>
2023-05-08 09:53:06 +0200

    Delete Unidad1_Ejercicio2 directory

--- Unidad1_Ejercicio2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿string firstName;
-firstName = "Bob";
-Console.WriteLine(firstName);
-firstName = "Liem";
-Console.WriteLine(firstName);
-firstName = "Isabella";
-Console.WriteLine(firstName);
-firstName = "Yasmin";
-Console.WriteLine(firstName);
